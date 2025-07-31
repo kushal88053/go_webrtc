52638f8417593a9249a2898aec4717abf2aca3c7
kushal88053 <91787820+kushal88053@users.noreply.github.com>
2025-08-01 00:46:09 +0530

    room created

--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -1,1 +1,37 @@
 package handlers
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+	guuid "github.com/google/uuid"
+)
+
+func RoomCreate(c *fiber.Ctx) error {
+	id := guuid.New().String()
+	return c.Redirect(fmt.Sprintf("/room/%s", id), fiber.StatusFound)
+
+}
+
+func Room(c *fiber.Ctx) error {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid room UUID")
+	}
+
+	uuid, suuid, _ := createOrGetRoom(uuid)
+}
+
+func RoomWebsocket(c *websocket.Conn) {
+
+	uuid := c.Params("uuid")
+
+	if uuid == "" {
+		c.Close(websocket.StatusBadRequest, "Invalid room UUID")
+		return
+	}
+
+	_, _, room := createOrGetRoom(uuid)
+
+}
